4bcd3bc37aeade5ba630fdbe0ae7f90cabc9716b
gameshalico <gameshalico@outlook.jp>
2023-12-25 17:06:05 +0900

    feat: Add Timer for control TimeCounter

---
+++ Runtime/Timer.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading;
+using Cysharp.Threading.Tasks;
+
+namespace TimeSpace
+{
+    public class Timer<TTimeCounter> where TTimeCounter : ITimeCounter, new()
+    {
+        private readonly IDeltaTimeSource _source;
+        private readonly PlayerLoopTiming _timing;
+        private TTimeCounter _timeCounter;
+        private CancellationTokenSource _cts;
+
+        public Timer(IDeltaTimeSource source, PlayerLoopTiming timing = PlayerLoopTiming.Update)
+        {
+            _timeCounter = new TTimeCounter();
+            _source = source;
+            _timing = timing;
+        }
+
+        public TTimeCounter TimeCounter => _timeCounter;
+
+        public float ElapsedTime => _timeCounter.ElapsedTime;
+
+        public void Start()
+        {
+            _cts?.Cancel();
+            _cts = new CancellationTokenSource();
+            _timeCounter.Run(_source, _timing, _cts.Token).Forget();
+        }
+
+        public void Stop()
+        {
+            _cts?.Cancel();
+        }
+    }
+}
